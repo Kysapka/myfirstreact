a22f8712130afdf7806677c5d12b176419c079bf
Kysapka <kysapka@gmail.com>
2021-06-25 23:10:48 +0300

    LESSON 16 PROPS

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from "./MyPosts.module.css";
 import Post from './Post/Post';
 
-const MyPosts = () => {
+const MyPosts = (props) => {
     return (<div>
             <div>
                 <textarea></textarea>
@@ -10,12 +10,9 @@ const MyPosts = () => {
 
             </div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post countlike="7" message= "Hi, how are you"/>
+                <Post countlike="12" message= "It's my first post" />
+
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import s from "./Post.module.css";
 
-const Post = () => {
-    return (
+const Post = (props) => {
+        return (
         <div className={s.item}>
             <img src="https://i.pinimg.com/736x/33/85/f2/3385f2e1ae928f80fda6304ce36c6165--avatar-makeup-film-avatar.jpg" />
-            post 1
+            {props.message}
             <div>
-                <span>like</span>
+                <span> {props.countlike} likes</span>
             </div>
         </div>
     )}
