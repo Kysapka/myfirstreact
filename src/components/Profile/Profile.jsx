a854cc5e03b83d74d6337d5df7a929f7ed357d2b
Kysapka <kysapka@gmail.com>
2021-06-28 01:47:03 +0300

    Verstka and NavLink and Brouser Router

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img
                     src="http://plantura.ru/wp-content/uploads/2018/02/%D0%9A%D1%83%D0%B1%D0%B0-%D1%87%D1%82%D0%BE-%D0%BD%D1%83%D0%B6%D0%BD%D0%BE-%D0%B7%D0%BD%D0%B0%D1%82%D1%8C.jpg"/>
