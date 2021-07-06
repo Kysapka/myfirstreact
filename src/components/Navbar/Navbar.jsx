3d82dcfaad6fd761a499dc925a7338df6b686ee9
Kysapka <kysapka@gmail.com>
2021-07-06 17:00:08 +0300

    CLASS_COMPONENT_INJECT

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,26 +5,29 @@ import Friends from './Friends/Friends';
 import FriendsContainer from './Friends/FriendsContainer';
 
 const Navbar = (props) => {
-    return (        <nav className={s.nav}>
-                    <div className={s.item}>
-                        <NavLink activeClassName={s.activeLink} to="/profile">Profile</NavLink>
-                    </div>
-                    <div className={`${s.item} ${s.activeLink}`}>
-                        <NavLink activeClassName={s.activeLink} to="/dialogs">Messages</NavLink>
-                    </div>
-                    <div className={s.item}>
-                        <NavLink activeClassName={s.activeLink} to="/news">News</NavLink>
-                    </div>
-                    <div className={s.item}>
-                        <NavLink activeClassName={s.activeLink} to="/music">Music</NavLink>
-                    </div>
-                    <div className={s.item}>
-                        <NavLink activeClassName={s.activeLink} to="/settings">Settings</NavLink>
-                    </div>
-                    <div>
-                        <FriendsContainer />
-                    </div>
-                </nav>
+    return (<nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink activeClassName={s.activeLink} to="/profile">Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink activeClassName={s.activeLink} to="/users">Users</NavLink>
+            </div>
+            <div className={`${s.item} ${s.activeLink}`}>
+                <NavLink activeClassName={s.activeLink} to="/dialogs">Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink activeClassName={s.activeLink} to="/news">News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink activeClassName={s.activeLink} to="/music">Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink activeClassName={s.activeLink} to="/settings">Settings</NavLink>
+            </div>
+            <div>
+                <FriendsContainer/>
+            </div>
+        </nav>
     )
 }
 
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,51 @@
 import React from "react";
 import styles from "./Users.module.css"
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png"
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers( [
-                {id: 1, photoUrl: "https://едем.рф/upload/thumbs/2020/10/05/2e0ff17ee6e225f4ca1c7829fb4dbd3e-256x256-crop.jpg", followed: false, fullName: "Dmitry", status: "I am s boss", location: {city: "Minsk", country: "Belarus"} },
-                {id: 2, photoUrl: "https://едем.рф/upload/thumbs/2020/10/05/2e0ff17ee6e225f4ca1c7829fb4dbd3e-256x256-crop.jpg", followed: true,  fullName: "Sasha", status: "I am s boss too", location: {city: "Moscow", country: "Russia"} },
-                {id: 3, photoUrl: "https://едем.рф/upload/thumbs/2020/10/05/2e0ff17ee6e225f4ca1c7829fb4dbd3e-256x256-crop.jpg", followed: false,  fullName: "Andrew", status: "I am s boss too", location: {city: "Kiev", country: "Ukraine"} },
-            ]
-        )
+
+class Users extends React.Component {
+
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
     }
 
+    render() {
+        return <div>
 
-
-
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto } className={styles.userPhoto}/>
                     </div>
                     <div>
                         { u.followed
-                            ? <button onClick={ ()=> { props.unfollow(u.id) } } >Unfollow</button>
-                            : <button onClick={ ()=> { props.follow(u.id) } } >Follow</button>}
+                            ? <button onClick={ ()=> {
+                                this.props.unfollow(u.id) } } >Unfollow</button>
+                            : <button onClick={ ()=> {
+                                this.props.follow(u.id) } } >Follow</button>}
                     </div>
                 </span>
-                <span>
                     <span>
-                        <div>{u.fullName}</div>
+                    <span>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)
-        }
+                </div>)
+            }
 
-    </div>
+        </div>
+    }
 }
+
 
 export default Users
